9c27f7f80d77b05164a23c80ba9fac131f4c0567
Clement M <massardc@users.noreply.github.com>
2017-08-15 21:00:11 +0200

    Added DataService and dynamical cells creation

---
+++ Coder Swag/Model/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  Coder Swag
+//
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
